fd675681b2b43457c587a42665394118c6b29765
coderoycc <112769866+Ruon2414@users.noreply.github.com>
2023-09-21 19:44:09 -0400

    Modificacion inicio database

---
+++ README.md
@@ -0,0 +1,9 @@
+# Laravel 8
+* Crear un proyecto con composer 
+* `composer create-project laravel/laravel nombreProy 8.*`
+* Ejecutar un proyecto `php artisan serve` el proyecto se mostrara en 8000
+
+## Comandos artisan
+* `php artisan route:list -c` ver las rutas de nuestra web 
+* `php artisan make:controller nombreControlador` crea un controlador en la carpeta app/controllers
+* 
---
+++ database/migrations/2023_09_21_231551_create_paciente_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePacienteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tblPaciente', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombres', 100);
+            $table->string('apellidos', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('paciente');
+    }
+}
